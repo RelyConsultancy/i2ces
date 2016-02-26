b566f1bae51826da4fad5dcea4a857170e35f728
Vlad Horecica <vlad.horecica@gmail.com>
2016-02-26 11:13:38 +0200

    IES 17 Created /me endpoint.

---
+++ backend/src/Evaluation/MeBundle/Controller/Api/Rest/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Evaluation\MeBundle\Controller\Api\Rest;
+
+use FOS\RestBundle\View\View;
+use Oro\Bundle\ApiBundle\Controller\RestApiController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Class UserController
+ *
+ * @package Evaluation\MeBundle\Controller
+ */
+class UserController extends RestApiController
+{
+    /**
+     * Returns a json with the current user data.
+     *
+     * @return View
+     */
+    public function getAction()
+    {
+        $usr = $this->getUser();
+        $data = array(
+            'id'             => $usr->getId(),
+            'username'       => $usr->getUsername(),
+            'business_units' => $usr->getBusinessUnits(),
+        );
+
+        return new JsonResponse($data);
+    }
+}
---
+++ backend/src/Evaluation/MeBundle/EvaluationMeBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Evaluation\MeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class EvaluationMeBundle
+ *
+ * @package Evaluation\MeBundle
+ */
+class EvaluationMeBundle extends Bundle
+{
+}
