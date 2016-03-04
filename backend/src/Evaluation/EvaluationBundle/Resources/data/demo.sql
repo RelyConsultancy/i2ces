8f4cac70563cbba4fe325ac8af2f27ee2804483f
Horia <horia.ardelean@freshbyteinc.com>
2016-03-04 16:58:57 +0200

    Changed sql order in dump.sql

--- backend/src/Evaluation/EvaluationBundle/Resources/data/demo.sql
+++ backend/src/Evaluation/EvaluationBundle/Resources/data/demo.sql
@@ -17,6 +17,16 @@ LIMIT 0, 1000
 INSERT INTO `evaluation` (`id`,`uid`,`title`,`start_date`,`end_date`,`generated_at`,`business_unit_id`,`category`,`brand`,`state`,`cid`) VALUES (1,'0f754e5c-6f77-4d55-a770-9d75d60c82e5','Lindt - Christmas 2015','2015-08-04 00:00:00','2016-01-21 00:00:00','2016-03-04 16:00:00',2,'IMPULSE FOOD','Lindor','draft','i2c1510047a');
 INSERT INTO `evaluation` (`id`,`uid`,`title`,`start_date`,`end_date`,`generated_at`,`business_unit_id`,`category`,`brand`,`state`,`cid`) VALUES (2,'248bf0d7-7d47-4330-acac-ff763a61d73e','SCA Plenty Genius Award','2015-08-04 00:00:00','2016-01-12 00:00:00','2016-03-04 16:00:00',3,'HOUSEHOLD & PETCARE','Plenty','draft','i2c1509134a');
 INSERT INTO `evaluation` (`id`,`uid`,`title`,`start_date`,`end_date`,`generated_at`,`business_unit_id`,`category`,`brand`,`state`,`cid`) VALUES (3,'da272b47-6966-4880-b8ad-9ddad624e223','Colgate Palmolive - CSPR','2015-07-13 00:00:00','2015-07-13 00:00:00','2016-03-04 16:00:00',3,'BABY & BEAUTY','Colgate','draft','i2c1507187a');
+
+/*
+-- Query: SELECT * FROM bap_standard.medium
+LIMIT 0, 1000
+
+-- Date: 2016-03-04 16:39
+*/
+INSERT INTO `medium` (`id`,`label`) VALUES (1,'email');
+INSERT INTO `medium` (`id`,`label`) VALUES (2,'paper');
+INSERT INTO `medium` (`id`,`label`) VALUES (3,'phone');
 /*
 -- Query: SELECT * FROM bap_standard.evaluation_chapters
 LIMIT 0, 1000
@@ -39,12 +49,3 @@ INSERT INTO `evaluation_mediums` (`evaluation_id`,`medium_id`) VALUES (1,2);
 INSERT INTO `evaluation_mediums` (`evaluation_id`,`medium_id`) VALUES (3,2);
 INSERT INTO `evaluation_mediums` (`evaluation_id`,`medium_id`) VALUES (2,3);
 INSERT INTO `evaluation_mediums` (`evaluation_id`,`medium_id`) VALUES (3,3);
-/*
--- Query: SELECT * FROM bap_standard.medium
-LIMIT 0, 1000
-
--- Date: 2016-03-04 16:39
-*/
-INSERT INTO `medium` (`id`,`label`) VALUES (1,'email');
-INSERT INTO `medium` (`id`,`label`) VALUES (2,'paper');
-INSERT INTO `medium` (`id`,`label`) VALUES (3,'phone');
