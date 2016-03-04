4e1e2d30530e856f6a4daad079c980b8d14ebf0c
Horia <horia.ardelean@freshbyteinc.com>
2016-03-04 16:54:57 +0200

    Added mysql data dumps for evaluations

---
+++ backend/src/Evaluation/EvaluationBundle/Resources/data/medium.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM bap_standard.medium
+LIMIT 0, 1000
+
+-- Date: 2016-03-04 16:39
+*/
+INSERT INTO `medium` (`id`,`label`) VALUES (1,'email');
+INSERT INTO `medium` (`id`,`label`) VALUES (2,'paper');
+INSERT INTO `medium` (`id`,`label`) VALUES (3,'phone');
