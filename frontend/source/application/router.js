54bd27286813de0313b0a87993603a22a748bcdd
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2016-03-06 20:04:02 +0200

    Switch router to hash history

--- frontend/source/application/router.js
+++ frontend/source/application/router.js
@@ -1,10 +1,10 @@
 import { createElement } from 'react'
-import { Router, browserHistory } from 'react-router'
+import { Router, browserHistory, hashHistory } from 'react-router'
 import routes from './routes.js'
 
 
 const router = createElement(Router, {
-  history: browserHistory,
+  history: hashHistory,
   routes,
 })
 
