754d611933230385165d91e18a6d3a8d462a9110
Horia <horia.ardelean@freshbyteinc.com>
2016-04-06 15:02:37 +0300

    Implemented the structure for custom css for the oro dashboard

---
+++ backend/src/i2c/ThemeBundle/i2cThemeBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace i2c\ThemeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class i2cThemeBundle
+ *
+ * @package i2c\ThemeBundle
+ */
+class i2cThemeBundle extends Bundle
+{
+}
