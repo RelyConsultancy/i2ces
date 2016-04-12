ab812bc1b89908bc7d54a57368e87665b27e8e62
vladhorecica <vlad.horecica@freshbyteinc.com>
2016-04-12 17:53:28 +0300

    Added sql queries for import all service.

--- backend/src/i2c/GenerateEvaluationBundle/Services/ImportAll.php
+++ backend/src/i2c/GenerateEvaluationBundle/Services/ImportAll.php
@@ -12,7 +12,7 @@ class ImportAll
     /** @var \PDO */
     protected $pdoConnection;
 
-    /** @var  string */
+    /** @var string */
     protected $databaseName;
 
     /**
@@ -38,13 +38,64 @@ class ImportAll
         $this->databaseName = $dbname;
     }
 
+    /**
+     * @param $importData
+     *
+     * @return mixed
+     */
     public function startImport($importData)
     {
-        // create row
+        $sql = sprintf(
+            'INSERT INTO i2c_import_version (version_number, start_date, config_data)
+             VALUES (%s, \'%s\', \'%s\')',
+            $importData['version_number'],
+            $this->getCurrentDate(),
+            json_encode($importData)
+        );
+
+        $result = $this->pdoConnection->query($sql)->fetch(\PDO::FETCH_COLUMN);
+
+        return $result;
     }
 
+    /**
+     * @param $importId
+     * @param $lastImportedFolder
+     *
+     * @return int
+     */
     public function endImport($importId, $lastImportedFolder)
     {
-        //update the row
+        $sql = sprintf(
+            'UPDATE i2c_import_version
+             SET end_date=\'%s\', last_import_date=\'%s\'
+             WHERE id=%s',
+            $this->getCurrentDate(),
+            $lastImportedFolder,
+            $importId
+        );
+
+        return $this->pdoConnection->exec($sql);
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastImportDate()
+    {
+        $sql = 'SELECT last_import_id
+                FROM i2c_import_version
+                ORDER BY id DESC
+                LIMIT 1';
+
+        return $this->pdoConnection->query($sql)->fetchColumn();
+    }
+
+    /**
+     * @return bool|string
+     */
+    protected function getCurrentDate()
+    {
+        return date('YYYY-MM-DD', time());
     }
 }
