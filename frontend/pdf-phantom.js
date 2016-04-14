73d652ee375654eb20ef69c4a4ada6dc93aadd68
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2016-04-14 23:48:00 +0300

    Add PDF render for phantomjs

---
+++ frontend/pdf-phantom.js
@@ -0,0 +1,44 @@
+console.log('Started Phantom.js')
+
+var system = require('system')
+var Page = require('webpage')
+
+var page = Page.create()
+var url = system.args[1]
+var filepath = system.args[2]
+var delay = system.args[3] || 0
+
+var dpi = 72.0
+var dpcm = dpi/2.05
+var pageWidth = 29.7
+var pageHeight = 21.0
+
+page.settings.dpi = dpi
+page.zoomFactor = 1
+
+page.viewportSize = {
+  width: Math.round(pageWidth * dpcm),
+  height: Math.round(pageHeight * dpcm)
+}
+
+page.paperSize = {
+  width: page.viewportSize.width + 'px',
+  height: page.viewportSize.height + 'px',
+  margin: '1cm'
+}
+
+page.open(url, function (status) {
+  if (status == "fail") {
+    page.close()
+    phantom.exit(1)
+    return
+  }
+
+  setTimeout(function () {
+    page.render(filepath, { format: 'pdf', quality: 100 })
+
+    console.log('Rendering complete')
+
+    phantom.exit()
+  }, delay)
+})
