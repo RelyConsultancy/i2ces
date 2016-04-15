84ef377db59ae90cb2006ca40745075f9f77b17b
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2016-04-15 11:56:46 +0300

    Add custom headers to phantomjs PDF renderer

--- frontend/pdf_phantom.js
+++ frontend/pdf_phantom.js
@@ -6,17 +6,26 @@ var Page = require('webpage')
 var page = Page.create()
 var url = system.args[1]
 var filepath = system.args[2]
-var delay = system.args[3] || 0
-
-console.log(url, filepath, delay)
+var headers = system.args[3]
+var delay = system.args[4] || 0
 
 var dpi = 72.0
 var dpcm = dpi/2.05
 var pageWidth = 29.7
 var pageHeight = 21.0
 
+// set request headers
+var customHeaders = {}
+var setHeader = function (string) {
+  var kv = string.trim().split('=')
+  customHeaders[kv[0]] = kv[1]
+}
+headers.split(',').forEach(setHeader)
+
+
 page.settings.dpi = dpi
 page.zoomFactor = 1
+page.customHeaders = customHeaders
 
 page.viewportSize = {
   width: Math.round(pageWidth * dpcm),
