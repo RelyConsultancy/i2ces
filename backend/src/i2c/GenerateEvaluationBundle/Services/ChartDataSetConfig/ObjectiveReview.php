bf730abcb2673bf36fdb92beaa7ce7769c6e62ed
jfairbrother <jfairbrother@AELAPJ7PJVZ1.LMUK.local>
2016-04-15 12:34:46 +0100

    Updated: ObjectiveReview Chart dataset configs added for objectives

--- backend/src/i2c/GenerateEvaluationBundle/Services/ChartDataSetConfig/ObjectiveReview.php
+++ backend/src/i2c/GenerateEvaluationBundle/Services/ChartDataSetConfig/ObjectiveReview.php
@@ -212,4 +212,271 @@ class ObjectiveReview implements ChartDataSetConfigInterface
             ],
         ];
     }
+    
+    /**
+     * @param $cid
+     *
+     * @return array
+     */
+    public function getLaunchNewProductConfig($cid)
+    {
+        $query = sprintf(
+            'SELECT COUNT(0) as count
+             FROM ie_results_data
+             WHERE media_type=\'Total\'
+             AND objective=\'Launch new product\'
+             AND metric=\'New_custs\'
+             AND product=\'Offer\'
+             AND timeperiod=2
+             AND master_campaign_id=\'%s\'
+            ',
+            $cid
+        );
+
+        $result = $this->connection->fetchAll($query);
+        
+        $this->logger->addCritical($result);
+        
+        if (1 > (int) $result[0]['count']) {
+            return [];
+        }
+
+        $query = sprintf(
+            'SELECT COUNT(0) as count
+             FROM ie_results_data
+             WHERE media_type=\'Total\'
+             AND objective=\'Launch new product\'
+             AND metric=\'New_custs\'
+             AND product=\'Offer\'
+             AND timeperiod=3
+             AND master_campaign_id=\'%s\'
+            ', 
+            $cid
+        );
+
+        $result = $this->connection->fetchAll($query);
+
+        if (1 > (int) $result[0]['count']) {
+            return [];
+        }
+
+        $query = sprintf(
+            'SELECT COUNT(0) as count
+             FROM ie_weekly_results_data
+             WHERE master_campaign_id = \'%s\'
+             AND product=\'Offer\'
+             AND metric=\'Units\'
+            ',
+            $cid
+        );
+
+        $result = $this->connection->fetchAll($query);
+
+        if (1 > (int) $result[0]['count']) {
+            return [];
+        }
+
+        return [
+            "launch_new_product" => [
+                "twig_name"    => "launch-new-product.json.twig",
+                "data_service" => "extract_chart_data_set_launch_new_product",
+            ],
+        ];
+    }
+    
+    public function getAcquireNewCustomersConfig ($cid)
+    {
+        
+        if ( $this->checkDoubleChartConfig($cid, 'Acquire new customers', 'New_custs') )
+        {
+            return [
+                "acquire_new_customers" => [
+                    "twig_name"    => "acquire-new-customers.json.twig",
+                    "data_service" => "extract_chart_data_set_acquire_new_customers",
+                ],
+            ];
+        }
+        
+        return [];
+        
+    }
+    
+    public function getRetainLapsingCustomersConfig ($cid)
+    {
+        
+        if ( $this->checkDoubleChartConfig($cid, 'Retain lapsing customers', 'Lapsed_custs') )
+        {
+            return [
+                "retain_lapsing_customers" => [
+                    "twig_name"    => "retain-lapsed-customers.json.twig",
+                    "data_service" => "extract_chart_data_set_retain_lapsed_customers",
+                ],
+            ];
+        }
+        
+        return [];
+        
+    }
+    
+    public function getRetainNewCustomersTrialistsConfig ($cid)
+    {
+        
+        if ( $this->checkDoubleChartConfig($cid, 'Retain new customers (trialists)', 'New_trialists') )
+        {
+            return [
+                "retain_new_customers_trialists" => [
+                    "twig_name"    => "retain-new-customers-trialists.json.twig",
+                    "data_service" => "extract_chart_data_set_retain_new_customers_trialists",
+                ],
+            ];
+        }
+        
+        return [];
+        
+    }
+    
+    public function getRetainExistingCustomersConfig ($cid)
+    {
+        
+        if ( $this->checkDoubleChartConfig($cid, 'Retain existing customers', 'Existing_custs') )
+        {
+            return [
+                "retain_existing_customers" => [
+                    "twig_name"    => "retain-existing-customers.json.twig",
+                    "data_service" => "extract_chart_data_set_retain_existing_customers",
+                ],
+            ];
+        }
+        
+        return [];
+        
+    }
+    
+    public function getGrowSpendPerExistingCustomerConfig ($cid)
+    {
+        
+        if ( $this->checkDoubleChartConfig($cid, 'Grow spend per existing customer', 'SPEC') )
+        {
+            return [
+                "grow_spend_per_existing_customer" => [
+                    "twig_name"    => "grow-spend-per-existing-customer.json.twig",
+                    "data_service" => "extract_chart_data_set_grow_spend_per_existing_customer",
+                ],
+            ];
+        }
+        
+        return [];
+        
+    }
+    
+    public function getGrowUnitsPerExistingCustomerConfig ($cid)
+    {
+        
+        if ( $this->checkDoubleChartConfig($cid, 'Grow units per existing customer', 'UPEC') )
+        {
+            return [
+                "grow_units_per_existing_customer" => [
+                    "twig_name"    => "grow-units-per-existing-customer.json.twig",
+                    "data_service" => "extract_chart_data_set_grow_units_per_existing_customer",
+                ],
+            ];
+        }
+        
+        return [];
+        
+    }
+    
+    public function getGrowFrequencyOfSharePerCustomerConfig ($cid)
+    {
+        
+        if ( $this->checkDoubleChartConfig($cid, 'Grow frequency of shop per customer', 'FOP') )
+        {
+            return [
+                "grow_frequency_of_share_per_customer" => [
+                    "twig_name"    => "grow-frequency-of-purchase.json.twig",
+                    "data_service" => "extract_chart_data_set_grow_frequency_of_purchase",
+                ],
+            ];
+        }
+        
+        return [];
+        
+    }
+    
+    public function getGrowCustomerProductRangeConfig ($cid)
+    {
+        
+        if ( $this->checkDoubleChartConfig($cid, 'Grow customer product range (cross sell)', 'GCS_custs') )
+        {
+            return [
+                "grow_customer_product_range" => [
+                    "twig_name"    => "grow-cross-sell.json.twig",
+                    "data_service" => "extract_chart_data_set_grow_cross_sell",
+                ],
+            ];
+        }
+        
+        return [];
+        
+    }
+    
+    public function getGrowShareOfCategoryConfig ($cid)
+    {
+        
+        if ( $this->checkDoubleChartConfig($cid, 'Grow share of category', 'SOW') )
+        {
+            return [
+                "grow_share_of_category" => [
+                    "twig_name"    => "grow-share-of-category.json.twig",
+                    "data_service" => "extract_chart_data_set_grow_share_of_category",
+                ],
+            ];
+        }
+        
+        return [];
+        
+    }
+    
+    private function checkDoubleChartConfig ($cid, $objective, $metric)
+    {
+        $query = sprintf(
+            'SELECT COUNT(0) as count
+             FROM ie_results_data
+             WHERE media_type=\'Total\'
+             AND objective=\'' . $objective . '\'
+             AND metric=\'' . $metric . '\'
+             AND product=\'Offer\'
+             AND timeperiod=2
+             AND master_campaign_id=\'%s\'
+            ',
+            $cid
+        );
+        
+        $result = $this->connection->fetchAll($query);
+
+        if (1 > (int) $result[0]['count']) {
+            return false;
+        }
+        
+        $query = sprintf(
+            'SELECT COUNT(0) as count
+             FROM ie_results_data
+             WHERE media_type=\'Total\'
+             AND objective=\'' . $objective . '\'
+             AND metric=\'' . $metric . '\'
+             AND product=\'Offer\'
+             AND timeperiod=3
+             AND master_campaign_id=\'%s\'
+            ',
+            $cid
+        );
+        
+        $result = $this->connection->fetchAll($query);
+
+        if (1 > (int) $result[0]['count']) {
+            return false;
+        }
+        
+        return true;
+    }
 }
