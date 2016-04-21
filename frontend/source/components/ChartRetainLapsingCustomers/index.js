ce842cd5cb6c3227d074bff41e52db5003970411
jfairbrother <jfairbrother@AELAPJ7PJVZ1.LMUK.local>
2016-04-21 14:18:14 +0100

    Updated retain lapsing customers chart

--- frontend/source/components/ChartRetainLapsingCustomers/index.js
+++ frontend/source/components/ChartRetainLapsingCustomers/index.js
@@ -1,39 +1,71 @@
-import { Component, B } from '/components/component.js'
+import { Component, B, Element } from '/components/component.js'
+import Grid from '/components/Grid'
 import Chart from '/components/Chart'
 import { fetchDataset } from '/application/actions.js'
 import d3 from 'd3'
 import style from './style.css'
+import numeral from 'numeral'
 
-
+const H3 = Element('h3')
 // a factory function for the chart
-const ChartRetainLapsingCustomers = (data) => {
-  // format data
-  const labels = data.map(i => i.label)
-  const uplift = data.map(i => i.uplift)
+const ChartRetainLapsingCustomers = (data, type) => {
+  
+  type = type || 'offer'
+  
+  const charts = {}
+  
+  charts.offer = {
+      exposed: data.charts.offer.map(i => i.exposed),
+      control: data.charts.offer.map(i => i.control)
+  }
+  
+  charts.brand = {
+      exposed: data.charts.brand.map(i => i.exposed),
+      control: data.charts.brand.map(i => i.control)
+  }
+  
+  console.log(charts);
 
   // below is a C3 chart
   const chart = Chart({
     type: 'bar',
     data: {
-      x: 'labels',
-      columns: [
-        ['labels'].concat(labels),
-        ['uplift'].concat(uplift),
-      ],
-      names: {
-        uplift: 'Units uplift',
-        percent: 'Weekly unit uplift/HH vs average',
+      type: 'bar',
+      x: 'Labels',
+      colors: {
+        'Control': '#A6A6A6',
+        'Exposed': '#7D3471'
       },
+      columns: [
+        ['Control'].concat(charts[type].control),
+        ['Exposed'].concat(charts[type].exposed),
+        ['Labels', 'During', 'Post']
+      ],
+      labels: {
+          format: (value) => {
+              return value
+          }
+      }
+    },
+    tooltip: {
+        show: false
     },
     axis: {
       x: {
-        type: 'category'
+        type: 'category',
+        categories: ['During', 'Post']
       },
       y: {
         label: {
-          text: 'lorem ipsum sit dolor',
-          position: 'outer-middle',
+          text: 'Number of lapsed customers',
+          position: 'outer-middle'
         },
+        tick: {
+            format: (value) => {
+                return value
+            }
+            
+        }
       },
     },
   })
@@ -51,14 +83,26 @@ export default Component({
     const { source } = this.props.component
 
     fetchDataset(source, (data) => {
+        
       this.setState({ data })
     })
   },
   render () {
     const { data } = this.state
-
-    if (data.length) {
-      return B({ className: style.chart }, ChartRetainLapsingCustomers(data))
+    if ('charts' in data) {
+        return Grid({
+          blocks: 2,
+          items: [
+              B(
+                H3({ className: 'i2c-chart-title' }, data.charts.offer.exposed[0] - data.charts.offer.control[0] + ' additional lapsed customer purchasing offer during campaign'),
+                B({ className: style.chart }, ChartRetainLapsingCustomers(data))
+              ),
+              B(
+                H3({ className: 'i2c-chart-title' }, data.charts.brand.exposed[0] - data.charts.brand.control[0] + ' additional lapsed customer purchasing brand during campaign'),
+                B({ className: style.chart }, ChartRetainLapsingCustomers(data, 'brand'))
+              )
+          ]
+      })
     }
     else {
       return B({ className: style.loading }, 'Loading data ...')
