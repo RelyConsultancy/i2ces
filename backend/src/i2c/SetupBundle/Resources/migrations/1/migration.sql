3a63d35b9f04baa55929ec01de1601f3d1e0f608
Horia <horia.ardelean@freshbyteinc.com>
2016-04-25 10:36:34 +0300

    Updated ansible for fonts and images integrationa and migration for latest_pdf_path

--- backend/src/i2c/SetupBundle/Resources/migrations/1/migration.sql
+++ backend/src/i2c/SetupBundle/Resources/migrations/1/migration.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS `i2c_evaluation` (
   generated_at     DATETIME,
   business_unit_id INT(11),
   version_number   VARCHAR(255),
+  latest_pdf_path  VARCHAR(255),
   FOREIGN KEY (business_unit_id) REFERENCES oro_business_unit (id),
   PRIMARY KEY (id)
 )
