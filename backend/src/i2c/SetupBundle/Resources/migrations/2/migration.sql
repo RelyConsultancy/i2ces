bb62a56c553f25e5d27d27a3a628ea20d62060a0
jfairbrother <jfairbrother@AELAPJ7PJVZ1.LMUK.local>
2016-05-24 13:57:53 +0100

    Updated migration 3 to include all units

--- backend/src/i2c/SetupBundle/Resources/migrations/2/migration.sql
+++ backend/src/i2c/SetupBundle/Resources/migrations/2/migration.sql
@@ -3,12 +3,3 @@ ALTER TABLE `i2c_evaluation` ADD COLUMN pdf_markers VARCHAR(255);
 ALTER TABLE `i2c_reimported_evaluation` ADD COLUMN temporary_pdf_path VARCHAR(255);
 ALTER TABLE `i2c_reimported_evaluation` ADD COLUMN pdf_markers VARCHAR(255);
 DROP TABLE i2c_generate_pdf_queue;
-
-
-INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('UPEC', 'units');
-INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('GCS_custs', 'customer');
-INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('Known_spend', 'GBP');
-INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('Lapsed_custs', 'customer');
-INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('OTS', 'units');
-INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('SOW', 'percentage');
-INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('Existing_custs', 'customer');
---
+++ backend/src/i2c/SetupBundle/Resources/migrations/3/migration.sql
@@ -0,0 +1,7 @@
+INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('UPEC', 'units');
+INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('GCS_custs', 'customer');
+INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('Known_spend', 'GBP');
+INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('Lapsed_custs', 'customer');
+INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('OTS', 'units');
+INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('SOW', 'percentage');
+INSERT INTO `i2c_objective_units` (`metric`, `unit`) VALUES ('Existing_custs', 'customer');
